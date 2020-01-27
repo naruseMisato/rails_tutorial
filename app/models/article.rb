921b1fba0f8b4975cd9967dc8b36a9f080c2859e
naruseMisato <misato.naruse@jmty.jp>
2020-01-27 20:25:00 +0900

    課題②を説いた

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,13 +8,13 @@ class Article
   field :pv_count, type: Integer
 
   # 課題①
-  /*
+=begin
   Article モデルのtitle に以下のバリデーションを記述する
   presence バリデーション
   メッセージ「タイトルは入力必須です」
   20文字length バリデーション
   メッセージ「タイトルは20文字以内で入力してください」
-  */
+=end
   # バリデーションヘルパー
   validates :title, presence:{message: "タイトルは入力必須です"},length: {maximum: 20, message: "タイトルは20文字以内で入力してください"}
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,18 @@ class Product
   include Mongoid::Timestamps
 
   field :name, type: String
+  field :errorStr, type: String
 
   validate :check_ng_word
 
+=begin
+  Article モデルのname プロパティにNGワードチェックバリデーションを追加する
+  NGワード
+  江頭、タイツ、露出狂、2:50
+  NGワードは配列でクラス定数で用意すること
+=end
   def check_ng_word
-    errors.add(:name, "使えない文字列が含まれています") if name&.eql? "なるせ"
+    errorStr = ["江頭", "タイツ", "露出狂", "2:50"]
+    errors.add(:name, "使えない文字列が含まれています") if errorStr.include? name
   end
 end
