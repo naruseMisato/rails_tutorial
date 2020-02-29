fd1d02f2720d9f57715cb834fbeb4edc05ed9b62
naruseMisato <misato.naruse@jmty.jp>
2020-02-29 14:07:03 +0900

    routingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   post "/piyo", to: "piyo#index"
+
+  # routingの設定
+  # get "/users", to: "users#index"
+  # post "/users", to: "users#create"
+  # patch "/users", to: "users#update"
+  # delete "/users", to: "users#destroy"
+  # get "/users", to: "users#show"
+
+  # # resourcesの設定
+  # resources :users, except: [:new, :edit]
+
+  # ネストの設定
+  resources :users, except: [:new, :edit] do
+    resources :articles do
+    end
+  end
 end
