7cb1941c859f2db69071db7536ea13751c4a1cfc
naruseMisato <misato.naruse@jmty.jp>
2020-03-02 11:33:12 +0900

    テストのコミット

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem "pry-byebug"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 3.0.0'
+  gem 'factory_girl_rails', '~> 3.0'
 end
 
 group :development do
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe Article, type: :model do
+  describe '#check_protection_organization' do
+    context '愛護団体ではない＆初めての投稿で里親カテゴリの場合' do
+      # userの初期化
+      it "成功する"　do
+        # user = User.create(name: "naruse", mail_address: "misato.naruse+11@jmty.jp", is_protection_organization: false)
+        # # 投稿作成
+        # first_article = user.articles.build(category_id: "2", title: "test")
+        # # validationを走らせて、それの結果をtrueと比較
+        # expect(first_article.valid?).to eq true
+      end
+    end
+  end
+end
