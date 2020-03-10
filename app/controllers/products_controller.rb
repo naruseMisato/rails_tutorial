56d6cb31dd0aa43b7a7c7544f32cd4405f45573b
naruseMisato <misato.naruse@jmty.jp>
2020-03-10 09:22:57 +0900

    微修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,12 @@
 class ProductsController < ApplicationController
     def index
+        # 固定データを渡す
         # @array = [
         #     {id: "5e664f52e513688cd0651dce", name: "iPhoneX 10000円", user: {id: "fhud29fkqmo2", name: "naru"}},
         #     {id: "5e664f52e513688cd0651dcf", name: "iPhoneX 20000円", user: {id: "fhud29fkqmo2", name: "naru"}}
         # ]
+
+        # mmodelからデータを渡す
         @array = Product.all
     end
 
